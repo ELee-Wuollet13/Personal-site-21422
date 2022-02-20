19b4ad8060e0ce02431e19dd602edc56612544dc
Everett Lee-Wuollet <everettlee-wuollet@Everetts-MacBook-Air.local>
2022-02-20 12:51:32 -0800

    add projects components

--- personal/src/components/App.js
+++ personal/src/components/App.js
@@ -7,10 +7,7 @@ function App(){
   return (
     <React.Fragment>
       <Header />
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+
     </React.Fragment>
   );
 }
